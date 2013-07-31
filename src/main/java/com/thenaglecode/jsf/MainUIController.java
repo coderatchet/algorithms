159f110d9ff52c0c21e7ceb88810fa14d7f135d1
= <=>
2013-07-31 19:10:35 +1000

    did some testing with jsf pages

---
+++ src/main/java/com/thenaglecode/jsf/MainUIController.java
@@ -0,0 +1,20 @@
+package com.thenaglecode.jsf;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jxnagl
+ * Date: 7/31/13
+ * Time: 1:58 PM
+ */
+
+@ManagedBean(name = "main")
+public class MainUIController {
+
+    private String magic;
+
+    public String getHelloWorld(){
+        return "hello world";
+    }
+}
---
+++ src/main/java/com/thenaglecode/jsf/NavigationController.java
@@ -0,0 +1,20 @@
+package com.thenaglecode.jsf;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jxnagl
+ * Date: 7/31/13
+ * Time: 2:48 PM
+ */
+@ManagedBean(name = "navigationController")
+public class NavigationController {
+    public String moveToPage1(){
+        return "page1";
+    }
+
+    public String moveToRandomGeneratorPage(){
+        return "random";
+    }
+}
